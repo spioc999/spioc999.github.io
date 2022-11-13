3ab2ed5ea1016df3d13805fd16b0ea965254d2eb
Simone Pio Caronia <spcaronia@gmail.com>
2022-11-13 19:04:09 +0100

    fix

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,9 +5,9 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "18c24ba374698a44585e3ad8b32c2212",
 "favicon.ico": "8a4331e80830d68c873306905cda7f8b",
-"index.html": "4fa4e39ca77bc4b7bbe239ab837e75b9",
-"/": "4fa4e39ca77bc4b7bbe239ab837e75b9",
-"main.dart.js": "316874aa789a38727576f6c990eae959",
+"index.html": "4a317c559280977c0a0ca7b4dcc01c23",
+"/": "4a317c559280977c0a0ca7b4dcc01c23",
+"main.dart.js": "a82e9ddbe030c94574166fa84b0e53f2",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
